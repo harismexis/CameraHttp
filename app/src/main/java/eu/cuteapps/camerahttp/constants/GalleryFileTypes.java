4387d81d0b23d2c402164a858c2240531964ab10
Haris Mexis <haris@fantoo.net>
2016-11-10 22:34:21 +0000

    clean and tidy

--- app/src/main/java/eu/cuteapps/camerahttp/constants/GalleryFileTypes.java
+++ app/src/main/java/eu/cuteapps/camerahttp/constants/GalleryFileTypes.java
@@ -5,5 +5,4 @@ public class GalleryFileTypes {
   public static final String TYPE_IMAGE = "image/*";
   public static final String TYPE_VIDEO = "video/*";
   public static final String TYPE_AUDIO = "audio/*";
-
 }
